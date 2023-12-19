3e5543f99db18cf8433488d9358ac3f75b6b61f0
kuby49 <imre.ruskal@gmail.com>
2023-12-19 15:46:02 +0100

    Update example_python_decorator.py

--- dags/example_python_decorator.py
+++ dags/example_python_decorator.py
@@ -43,7 +43,7 @@ PATH_TO_PYTHON_BINARY = sys.executable
 with DAG(
     dag_id="example_python_operator",
     schedule=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"), 
     catchup=False,
     tags=["example"],
 ):
